f799e9f4485035361288a9d5bb672f9b07b92c4c
Haky86 <hakamelassouad@gmail.com>
2023-11-16 21:07:47 +0100

    a71: overlay: Move UDFPS overlays to the right place
    Change-Id: Ib4cd7f5c63ad2b9d8be9a1171e4a2a715dd7c09f

--- device.mk
+++ device.mk
@@ -43,7 +43,6 @@ $(call inherit-product, device/samsung/sm6150-common/sm6150.mk)
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
